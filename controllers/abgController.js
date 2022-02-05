e8650405e8a58b56545db19d76c1b92f0054b836
Dyanni Bigham <dyanniham@gmail.com>
2022-02-05 12:21:15 -0700

    start controller code

---
+++ controllers/abgController.js
@@ -0,0 +1,13 @@
+exports.index = function(req, res) {
+    // TODO: update later when we have this data
+    // but make this a 'Bad Request' since it won't be
+    // a valid HTTP request
+    res.send("NOT IMPLEMENTED: Still need ABG data");
+};
+
+exports.retrieve_ABG = function(req,res) {
+    //TODO: update later when we have this data
+    // This will be used to parse the request
+    // to get the right ABG tile
+    res.send("NOT IMPLEMENTED: Still need ABG data");
+};
---
+++ controllers/demController.js
@@ -0,0 +1,13 @@
+exports.index = function(req,res) {
+    // TODO: update later when we have this data
+    // but make this a 'Bad Request' since it won't be 
+    // a valid HTTP request
+    res.send("NOT IMPLEMENTED: Still need DEM data");
+};
+
+exports.retrieveDEM = function(req, res) {
+    // TODO: update later when we have this data
+    // THis will be used to parse the request
+    // to get the right DEM tile
+    res.send("NOT IMPLEMENTED: Still need DEM data");
+};
