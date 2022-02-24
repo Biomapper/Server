d239546e57300de0838a33f05d5115217117ee9c
Dyanni-Bigham <dyanniham@gmail.com>
2022-02-24 16:15:51 -0700

    add status code and error message

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,5 +2,7 @@
 exports.index = function(req, res) {
     // Sends a Bad Request status code since the server 
     // is expecting a certain request
-    res.status(400).send("Not enough information provided to the server.");
+    res.status(404).send("<title>404 Not Found</title> \
+         <h1>Not Found</h1> \
+          <p> Missing information to request tiles.");
 }
